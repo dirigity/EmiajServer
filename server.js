96d523d71ff86b411262545b60848507dd3fce2d
Jaime <jaimegonzalezfabregas@gmail.com>
2021-04-07 19:17:54 +0200

    vas

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ function tack() {
 
 tick()
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 7000);
 const server = app.listen(app.get('port'), () => {
     console.log(`Express running → PORT ${server.address().port}`);
 });
